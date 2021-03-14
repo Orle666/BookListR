61fcbf7a5bff622af22d2eba9649415e5d069470
odurdevic <orle.durdevic@infobip.com>
2021-03-14 21:17:57 +0100

    Stage all up to vl 29

---
+++ BookListRazor/Model/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        [Key] //znaci da je primary key da
+        public int Id { get; set; }
+        
+        [Required] // znaci da ne moze biti null
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+    }
+}
